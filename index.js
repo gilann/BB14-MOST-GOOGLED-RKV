e818d13ef0fa4d39665bfc0bc9c9a177813e830c
Shivam Gupta <mad.shivam111@gmail.com>
2020-12-23 09:47:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16294,9 +16294,9 @@ for(let i = 0; i<1638; i++){
 
   button.id = i ; 
   button.addEventListener("click", function() {
-    myFunction(qoutes[i]+"\n#BBTrendMasterRahulVaidya", i);
+    myFunction(qoutes[i]+"\n#RahulIsTheBoss", i);
   });
-  li.appendChild(document.createTextNode(i+1 + ". " + qoutes[i] +"\n#BBTrendMasterRahulVaidya"));
+  li.appendChild(document.createTextNode(i+1 + ". " + qoutes[i] +"\n#RahulIsTheBoss"));
   li.appendChild(button);
   x.appendChild(li);
 }
